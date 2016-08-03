2d652f7bf18dc2209554907dccb531106df5f556
Dave Michelson <daveism@gmail.com>
2016-08-03 14:40:15 -0400

    just use distribution bundle from dev

--- app/reducers/getMenuLists.js
+++
@@ -1,9 +0,0 @@
-export function menuLists(state = [], action) {
-  switch (action.type) {
-    case 'GET_MENU_LIST':
-      return { ...state, lists: action.lists}
-    default:
-      return state
-  }
-  return state;
-}
